b4b77683cd552c9e3d13975ae1bee7b0ab51bb70
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-05-25 15:04:52 +0200

    Fixes for bookmark actions sheet

--- lib/widgets/bookmark_item/bookmark_context_menu.dart
+++ lib/widgets/bookmark_item/bookmark_context_menu.dart
@@ -31,106 +31,93 @@ class BookmarkContextMenu extends ConsumerWidget {
     showModalBottomSheet(
       context: context,
       isDismissible: true,
+      showDragHandle: true,
       builder: (ctx) => SafeArea(
         bottom: true,
         top: true,
-        child: Wrap(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Wrap(
+            children: [
+              ListTile(
+                leading: Icon(
+                  bookmark.unread == true ? Icons.mark_email_read_rounded : Icons.mark_as_unread_rounded,
+                ),
+                title: Text(
+                  bookmark.unread == true
+                      ? t.bookmarks.bookmarkOptions.markAsRead
+                      : t.bookmarks.bookmarkOptions.markAsUnread,
+                ),
+                onTap: () {
+                  Navigator.pop(ctx);
+                  onReadUnread(bookmark);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  bookmark.isArchived == true ? Icons.unarchive_rounded : Icons.archive_rounded,
+                ),
+                title: Text(
+                  bookmark.isArchived == true
+                      ? t.bookmarks.bookmarkOptions.unarchive
+                      : t.bookmarks.bookmarkOptions.archive,
+                ),
+                onTap: () {
+                  Navigator.pop(ctx);
+                  onArchiveUnarchive(bookmark);
+                },
+              ),
+              ExpansionTile(
+                leading: const Icon(Icons.share_rounded),
+                title: Text(t.bookmarks.bookmarkOptions.shareOptions),
                 children: [
-                  Container(
-                    width: 50,
-                    height: 4,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(20),
+                  ListTile(
+                    contentPadding: const EdgeInsets.only(left: 44),
+                    leading: const Icon(Icons.input_rounded),
+                    title: Text(
+                      bookmark.shared == true
+                          ? t.bookmarks.bookmarkOptions.unshareInternally
+                          : t.bookmarks.bookmarkOptions.shareInternally,
                     ),
+                    onTap: () {
+                      Navigator.pop(ctx);
+                      onShareInternally(bookmark);
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.only(left: 44),
+                    leading: const Icon(Icons.output_rounded),
+                    title: Text(t.bookmarks.bookmarkOptions.shareThirdPartyApp),
+                    onTap: () {
+                      Navigator.pop(ctx);
+                      Share.share(bookmark.url!);
+                    },
                   ),
                 ],
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                bookmark.unread == true ? Icons.mark_email_read_rounded : Icons.mark_as_unread_rounded,
+              ListTile(
+                leading: const Icon(Icons.edit_rounded),
+                title: Text(t.bookmarks.bookmarkOptions.edit),
+                onTap: () {
+                  Navigator.pop(ctx);
+                  onEdit(bookmark);
+                },
               ),
-              title: Text(
-                bookmark.unread == true
-                    ? t.bookmarks.bookmarkOptions.markAsRead
-                    : t.bookmarks.bookmarkOptions.markAsUnread,
-              ),
-              onTap: () {
-                Navigator.pop(ctx);
-                onReadUnread(bookmark);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                bookmark.isArchived == true ? Icons.unarchive_rounded : Icons.archive_rounded,
-              ),
-              title: Text(
-                bookmark.isArchived == true
-                    ? t.bookmarks.bookmarkOptions.unarchive
-                    : t.bookmarks.bookmarkOptions.archive,
-              ),
-              onTap: () {
-                Navigator.pop(ctx);
-                onArchiveUnarchive(bookmark);
-              },
-            ),
-            ExpansionTile(
-              leading: const Icon(Icons.share_rounded),
-              title: Text(t.bookmarks.bookmarkOptions.shareOptions),
-              children: [
-                ListTile(
-                  contentPadding: const EdgeInsets.only(left: 44),
-                  leading: const Icon(Icons.input_rounded),
-                  title: Text(
-                    bookmark.shared == true
-                        ? t.bookmarks.bookmarkOptions.unshareInternally
-                        : t.bookmarks.bookmarkOptions.shareInternally,
-                  ),
-                  onTap: () {
-                    Navigator.pop(ctx);
-                    onShareInternally(bookmark);
-                  },
+              ListTile(
+                leading: Icon(
+                  Icons.delete_rounded,
+                  color: Theme.of(ctx).colorScheme.error,
                 ),
-                ListTile(
-                  contentPadding: const EdgeInsets.only(left: 44),
-                  leading: const Icon(Icons.output_rounded),
-                  title: Text(t.bookmarks.bookmarkOptions.shareThirdPartyApp),
-                  onTap: () {
-                    Navigator.pop(ctx);
-                    Share.share(bookmark.url!);
-                  },
+                title: Text(
+                  t.bookmarks.bookmarkOptions.delete,
+                  style: TextStyle(color: Theme.of(ctx).colorScheme.error),
                 ),
-              ],
-            ),
-            ListTile(
-              leading: const Icon(Icons.edit_rounded),
-              title: Text(t.bookmarks.bookmarkOptions.edit),
-              onTap: () {
-                Navigator.pop(ctx);
-                onEdit(bookmark);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.delete_rounded,
-                color: Theme.of(ctx).colorScheme.error,
+                onTap: () {
+                  Navigator.pop(ctx);
+                  onDelete(bookmark);
+                },
               ),
-              title: Text(
-                t.bookmarks.bookmarkOptions.delete,
-                style: TextStyle(color: Theme.of(ctx).colorScheme.error),
-              ),
-              onTap: () {
-                Navigator.pop(ctx);
-                onDelete(bookmark);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
